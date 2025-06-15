3937cbd51d2c4c31c45959917cca5689c2c98456
Surendrabohara <chhetrishaurav@gmail.com>
2025-06-15 17:47:22 +0545

    updated data visulazation

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -74,20 +74,92 @@ del gyr_df['elapsed (s)']
 # Turn into function
 # --------------------------------------------------------------
 files = glob("../../data/raw/MetaMotion/*.csv")
+def read_data_from_files(files):
+    acc_df = pd.DataFrame()
+    gyr_df = pd.DataFrame()
+    acc_set = 1
+    gyr_set = 1
+    for f in normalized_file_path:
+        participant =f.split("-")[0].replace(data_path,"")
+        label = f.split("-")[1]
+        category = f.split("-")[2].rstrip("123").rstrip("_MetaWear_2019")
+        df = pd.read_csv(f)
+        df['participant'] = participant
+        df['category'] = category
+        df['label'] = label
+        if 'Accelerometer' in f:
+            df['set'] = acc_set
+            acc_set += 1
+            acc_df = pd.concat([acc_df, df])
+        if 'Gyroscope' in f:
+            df['set'] = gyr_set
+            gyr_set += 1
+            gyr_df = pd.concat([gyr_df, df])
+    acc_df.index = pd.to_datetime(acc_df["epoch (ms)"], unit = 'ms')
+    gyr_df.index = pd.to_datetime(gyr_df["epoch (ms)"], unit = 'ms')
+    # removing the  unwanted columne 
+    del acc_df['epoch (ms)']
+    del acc_df['time (01:00)']
+    del acc_df['elapsed (s)']
+
+    del gyr_df['epoch (ms)']
+    del gyr_df['time (01:00)']
+    del gyr_df['elapsed (s)']
+    
+    return acc_df, gyr_df
+acc_df, gyr_df = read_data_from_files(files)
 
 # --------------------------------------------------------------
 # Merging datasets
 # --------------------------------------------------------------
+data_merged= pd.concat([acc_df.iloc[:,:3], gyr_df], axis=1)
+# dropping nan data filed 
+data_merged.dropna()
 
-
+# --------------------------------------------------------------
+# Rename Merged data columns
+# --------------------------------------------------------------
+data_merged.columns = [
+    'acc_x',
+    'acc_y',
+    'acc_z',
+    'gry_x',
+    'gry_y',
+    'gry_z',
+    'participant',
+    'category',
+    'label',
+    'set',
+]
 # --------------------------------------------------------------
 # Resample data (frequency conversion)
 # --------------------------------------------------------------
-
 # Accelerometer:    12.500HZ
 # Gyroscope:        25.000Hz
+sampling = {
+    'acc_x': 'mean',
+    'acc_y': 'mean',
+    'acc_z': 'mean',
+    'gry_x': 'mean',
+    'gry_y': 'mean',
+    'gry_z': 'mean',
+    'participant': 'last',
+    'category': 'last',
+    'label': 'last',
+    'set': 'last', 
+}
+
+data_merged[:1000].resample(rule='200ms').apply(sampling)
+
+# split data by day
+days = [g for n, g in data_merged.groupby(pd.Grouper(freq='D'))]
+data_resampled = pd.concat([df.resample(rule='200ms').apply(sampling).dropna() for df in days])
+data_resampled['set'] = data_resampled['set'].astype('int')
+data_resampled.info()
+
 
 
 # --------------------------------------------------------------
 # Export dataset
 # --------------------------------------------------------------
+data_resampled.to_pickle("../../data/interim/01_data_processed.pkl")
